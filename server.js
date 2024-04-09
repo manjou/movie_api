16001fec4e3b2c3de2532b48d715706eb5df8a0f
manjou <103428887+manjou@users.noreply.github.com>
2024-04-09 08:43:43 +0200

    Update server.js
    Added endpoint for:
    - getting a single user by ID

--- server.js
+++ server.js
@@ -164,6 +164,62 @@ app.get(
     }
   );
 
+// GET One User by ID
+// DELETE User
+app.get(
+  '/users/:id', 
+  passport.authenticate('jwt', { session: false }), 
+  async (req, res) => {
+   // CONDITION TO CHECK USER AUTHORIZATION
+  //  if(req.user.Username !== req.params.Username){
+  //   return res.status(400).send('Permission denied');
+  //   }
+    // CONDITION ENDS
+    await Users.findOne({ _id: req.params.id })
+      .then((user) => {
+        if (!user) {
+          res.status(400).send(req.params.Username + ' was not found.');
+          res.status(400).send('User with id ' + req.params.id + ' was not found.');
+        } else {
+          res.status(200).send(user.Username + ' was deleted.');
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
+  }
+);
+
+
+
+// DELETE User
+app.delete(
+  '/users/:id', 
+  passport.authenticate('jwt', { session: false }), 
+  async (req, res) => {
+   // CONDITION TO CHECK USER AUTHORIZATION
+  //  if(req.user.Username !== req.params.Username){
+  //   return res.status(400).send('Permission denied');
+  //   }
+    // CONDITION ENDS
+    await Users.findOneAndDelete({ _id: req.params.id })
+      .then((user) => {
+        if (!user) {
+          res.status(400).send(req.params.Username + ' was not found.');
+          res.status(400).send('User with id ' + req.params.id + ' was not found.');
+        } else {
+          res.status(200).send(user.Username + ' was deleted.');
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
+  }
+);
+
+
 // Update User data, by ID
 /* We´ll expect JSON in this format:
 {
@@ -296,32 +352,6 @@ app.delete(
   }
 );
 
-
-// DELETE User
-app.delete(
-  '/users/:id', 
-  passport.authenticate('jwt', { session: false }), 
-  async (req, res) => {
-   // CONDITION TO CHECK USER AUTHORIZATION
-  //  if(req.user.Username !== req.params.Username){
-  //   return res.status(400).send('Permission denied');
-  //   }
-    // CONDITION ENDS
-    await Users.findOneAndDelete({ _id: req.params.id })
-      .then((user) => {
-        if (!user) {
-          res.status(400).send(req.params.Username + ' was not found.');
-          res.status(400).send('User with id ' + req.params.id + ' was not found.');
-        } else {
-          res.status(200).send(user.Username + ' was deleted.');
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
-      });
-  }
-);
 
 
 // MOVIES ENDPOINTS ------------------------------------------------------------
